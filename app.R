ebe0091c44f69b8254002ae157a1e63de4e96b55
HAJAMARA <102218670+HAJAMARA@users.noreply.github.com>
2022-12-24 14:59:38 +0100

    Update app.R

--- app.R
+++ app.R
@@ -1,10 +1,12 @@
 
 library(shiny)
 library(shinyalert)
-library(bs4Dash)
+library(shinydashboard)
 library(shinyWidgets)
+library(bs4Dash)
 library(pastecs)
 library(readxl)
+library(openxlsx)
 library(DT)
 library(plotly)
 library(tidyverse)
@@ -14,8 +16,8 @@ library(ggplot2)
 library(data.table)
 library(tolerance)
 library(BH)
-
 launchApp <- function() {
   shinyApp(ui = ui, server = server)
 }
+
 
